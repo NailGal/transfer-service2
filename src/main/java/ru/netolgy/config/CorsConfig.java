1c426789ed8fbf74b539bed4571ead50239fa1de
NailGal <galiyllin@gmail.com>
2025-07-16 14:56:51 +0400

    CROS Error-add class CorsConfig

---
+++ src/main/java/ru/netolgy/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package ru.netolgy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://serp-ya.github.io") // Разрешенный origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
